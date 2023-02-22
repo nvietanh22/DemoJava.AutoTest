90dbddd73ec3b72ed76bc1406d1ba51d3c736ac9
anh.nguyenviet10 <anh.nguyenviet10>
2023-02-22 16:35:57 +0700

    Bài tập sắp xếp mảng tăng dần

--- DemoJava/src/main/java/demo/autotest/SapXepTangDan.java
+++ DemoJava/src/main/java/demo/autotest/SapXepTangDan.java
@@ -6,37 +6,36 @@ public class SapXepTangDan {
 
     public static void main(String[] args) {
         int number;
+        int[] soNguyen;
         System.out.print("Nhập số phần tử của mảng: ");
         Scanner scanner = new Scanner(System.in);
         number = scanner.nextInt();
-        // khởi tạo array
-        int[] array;
-        array = new int[number];
+        // Gán phần tử vào soNguyen
+        soNguyen = new int[number];
         System.out.print("Nhập các phần tử của mảng: \n");
         for (int i = 0; i < number; i++) {
-            System.out.printf("a[%d] = ", i);
-            array[i] = scanner.nextInt();
+            soNguyen[i] = scanner.nextInt();
         }
         // sắp xếp dãy số theo thứ tự tăng dần
         /**
          * sắp xếp mảng số nguyên theo thứ tự tăng dần
          *
-         * @param array: mảng các số nguyên
+         * @param soNguyen: mảng các số nguyên
          * @param number:   số phần tử của mảng
          */
         int temp;
         for (int i = 0; i < number - 1; i++) {
             for (int j = i + 1; j < number; j++) {
-                if (array[i] > array[j]) {
-                    temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
+                if (soNguyen[i] > soNguyen[j]) {
+                    temp = soNguyen[j];
+                    soNguyen[j] = soNguyen[i];
+                    soNguyen[i] = temp;
                 }
             }
         }
         System.out.println("Dãy số được sắp xếp tăng dần: ");
         for (int i = 0; i < number; i++) {
-            System.out.print(array[i] + " ");
+            System.out.print(soNguyen[i] + " ");
         }
     }
 }
